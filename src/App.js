526d2ee0f0ba9ebc1c4aa802ec7ca21744488363
CasualCoder25 <h.rajanna2505@gmail.com>
2023-10-13 18:13:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Body from "./componenets/Body"
 
 function App() {
+  const AppStyle = {
+    width: "100%",
+    minHeight:"100vh"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={AppStyle}>
+      <Body />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/componenets/Body.js
@@ -0,0 +1,241 @@
+import "./Body.css"
+
+export function Body() {
+  const BodyStyle = {
+    width: "100%",
+    minHeight: "60vh",
+    background:
+      "linear-gradient(135deg,lightblue 0%,darkblue 35%,transparent 50%,blue 300%),linear-gradient(#0088ff,blue,black 100vh)",
+  }
+  const SvgStyle = {
+    width: "100%",
+  }
+  return (
+    <div style={BodyStyle}>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="bg-svg-blue p-5 text-center text-light">
+        <h1>Sagar S</h1>
+        <div className="h4">Vellore Institute of Technology</div>
+        <div className="h4">
+          Branch Representative | Branch Topper 2022-23 | Core Committee Member
+          at ISTE VIT Vellore
+        </div>
+        <div className="h4">
+          Email: <a href="/#">h.rajanna2505@gmail.com</a>
+        </div>
+        <div className="h4">
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/sagar-s-bangalore/"
+            target="blank"
+          >
+            https://www.linkedin.com/in/sagar-s-bangalore/
+          </a>
+        </div>
+        <div className="h4">
+          GitHub:{" "}
+          <a href="https://github.com/CasualCoder25" target="blank">
+            https://github.com/CasualCoder25
+          </a>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5">
+        <div className="text-center text-light px-5">
+          <h1>About</h1>
+          <div>
+            I'm Sagar S, a dedicated and forward-thinking professional with a
+            dual passion for data science and web development. With a solid
+            foundation in coding and a keen interest in creating impactful
+            digital solutions, I'm excited to contribute my skills to dynamic
+            teams and innovative projects. I am the branch topper of Computer
+            Science Engineering with specialization in IOT branch in the
+            prestigious Vellore Institute of Technology. I have achieved a CGPA
+            of 9.87 and have gained insightful knowledge in the fields of
+            networking and database management by understanding various concepts
+            in VIT's meticulously planned course curriculum. Throughout my three
+            years of exploration and learning, I've honed my proficiency in
+            programming languages like Java ,C++ ,Python and C. This expertise
+            not only empowers me to analyze intricate datasets but also enables
+            me to craft intuitive coding solutions that integrate my knowledge
+            of data-structures and Algorithms. My commitment to continuous
+            learning has led me to gain hands-on experience in machine learning,
+            and other coding domains. As a student, I am very passionate about
+            Web development ,Machine learning and deep learning and have been
+            working hard to gain knowledge in the field.
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5">
+        <div className="text-center text-light px-5">
+          <h1>Experience</h1>
+          <div>
+            <div className="p-5">
+              <h2>Indian Society for Technical Education</h2>
+              <h4>9 months</h4>
+              <h4>Vellore, Tamil Nadu, India</h4>
+              <div className="h6">Machine learning Tech support</div>
+              <div>May 2023 - Present(On-site)</div>
+              <p>
+                During my tenure at ISTE, I have learnt to apply my skills in
+                Machine Learning and Deep Learning domains and have successfully
+                assisted various participants who attended the event Automate.py
+                that was held in VIT by our chapter.During my tenure at ISTE, I
+                have learnt to apply my skills in Machine Learning and Deep
+                Learning domains and have successfully assisted various
+                participants who attended the event Automate.py that was held in
+                VIT by our chapter.
+              </p>
+              <p>
+                Skills: Python (Programming Language) · Pandas (Software) ·
+                NumPy · Scikit-Learn
+              </p>
+            </div>
+            <div className="p-5">
+              <div className="h6">Core Committee Member</div>
+              <div>Feb 2023 - Present(Full-Time)</div>
+              <p>
+                Currently Senior Core Member and part of the Machine Learning
+                and Competitive Coding Domains. I have also gained a lot of
+                managerial skills thanks to ISTE's methodologies of equipping
+                its members with relevant and important management skills
+                irrespective of the members' domains.Currently Senior Core
+                Member and part of the Machine Learning and Competitive Coding
+                Domains.
+              </p>
+              <p>
+                {" "}
+                I have also gained a lot of managerial skills thanks to ISTE's
+                methodologies of equipping its members with relevant and
+                important management skills irrespective of the members'
+                domains.
+              </p>
+              <p>
+                Skills: Engineering · English · Communication · Strategy ·
+                Analytical Skills
+              </p>
+            </div>
+            <div className="p-5">
+              <h2>Vellore Institute of Technology</h2>
+              <h4>Jul 2022 - Present · 1 yr 4 mos</h4>
+              <h4>Vellore, Tamil Nadu, India(On-site)</h4>
+              <div className="h6">Program Representative</div>
+              <p>
+                I have been nominated as the program representative(PR) of my
+                branch of study due to my academic excellence. My role was to
+                understand and report the proceedings of academic activities and
+                convey insightful feedback collected from my peers to Student
+                Welfare Office at VIT Vellore. Over the course of my tenure, I
+                have understood the importance of efficient and effective
+                management and have acquired insightful knowledge, skill and
+                experience in the field.
+              </p>
+              <p>Skills: Leadership</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L48,117.3C96,139,192,181,288,176C384,171,480,117,576,96C672,75,768,85,864,101.3C960,117,1056,139,1152,154.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
+      <div className="d-flex justify-content-center px-5">
+        <div className="text-center text-light px-5">
+          <h1>Education</h1>
+          <div className="p-5">
+            <h2>Vellore Institute of Technology</h2>
+            <h4>Bachelor of Technology - BTech, Computer Science</h4>
+            <h4>Grade: 9.90 CGPA</h4>
+            <h4>Vellore, Tamil Nadu, India(On-site)</h4>
+          </div>
+          <div>
+            <p>
+              Programme representative of CSE IOT. I'm currently the programme
+              representative in VIT for the branch Computer science and
+              engineering with specialization in IOT
+            </p>
+            <p>
+              Skills: Java · Data Structures and Algorithms · SQL · Problem
+              Solving · Object-Oriented Programming (OOP) · C (Programming
+              Language) · C++ · Timing · Computer Science · Engineering ·
+              English · Communication · Strategy · Analytical Skills
+            </p>
+          </div>
+          <div className="p-5">
+            <h2>Deeksha - Center for Learning</h2>
+            <h4>Pre - University degree, PCMB</h4>
+            <h4>Grade: 100%</h4>
+            <h4>Bangalore, Karnataka, India(On-site)</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  )
+}
+export default Body
